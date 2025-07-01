16305a934342249213302d45908999d44b6e2491
shock59 <42130427+shock59@users.noreply.github.com>
2025-07-01 22:54:26 +1000

    Basic video player setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,66 @@
 <script lang="ts">
+  let video: HTMLVideoElement;
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+  <div id="video-container">
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <video bind:this={video}>
+      <source
+        src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        type="video/mp4"
+      />
+    </video>
 
-  <p>svelte electron app</p>
+    <div id="caption-container">
+      <span class="captions">Hello, world!</span>
+    </div>
+  </div>
+
+  <br />
+
+  <button
+    onclick={() => {
+      if (video.paused || video.ended) video.play();
+      else video.pause();
+    }}
+    >{#if video?.paused || video?.ended}Play{:else}Pause{/if}</button
+  >
 </main>
+
+<style>
+  * {
+    --videoWidth: 1280px;
+  }
+
+  #video-container {
+    width: var(--videoWidth);
+    height: calc(var(--videoWidth) * (9 / 16));
+    anchor-name: --videoContainer;
+  }
+
+  video {
+    width: 100%;
+  }
+
+  #caption-container {
+    position-anchor: --videoContainer;
+    position: fixed;
+    bottom: calc(anchor(bottom) + 8px);
+
+    width: var(--videoWidth);
+    display: flex;
+    justify-content: center;
+  }
+
+  .captions {
+    width: fit-content;
+    background: rgba(8, 8, 8, 0.75);
+    color: #ffffff;
+    font-family: "Roboto";
+    padding: 0 0.25em;
+    white-space: pre-wrap;
+    display: inline-block;
+    font-size: 32px;
+  }
+</style>
