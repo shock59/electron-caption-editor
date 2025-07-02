7ef18ff3f51c4e3d063f631da7ef7ab2d43e4a3e
shock59 <42130427+shock59@users.noreply.github.com>
2025-07-02 23:39:58 +1000

    Base video player functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,75 @@
 <script lang="ts">
-  let video: HTMLVideoElement;
+  import Icon from "@iconify/svelte";
+
+  let video: HTMLVideoElement = $state();
+  let playing: boolean = $state(false);
+
+  function togglePlayback() {
+    if (playing) video.pause();
+    else video.play();
+    playing = !playing;
+  }
 </script>
 
 <main>
   <div id="video-container">
-    <!-- svelte-ignore a11y_media_has_caption -->
-    <video bind:this={video}>
-      <source
-        src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-        type="video/mp4"
-      />
-    </video>
+    <div id="player-container">
+      <!-- svelte-ignore a11y_media_has_caption -->
+      <video bind:this={video}>
+        <source
+          src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        />
+      </video>
 
-    <div id="captions-container">
-      <div class="captions">
-        <div class="caption-line">Hello, world!</div>
-        <div class="caption-line">Line 2</div>
+      <div id="captions-container">
+        <div class="captions">
+          <div class="caption-line">Hello, world!</div>
+          <div class="caption-line">Line 2</div>
+        </div>
       </div>
     </div>
+
+    <div id="button-row">
+      <button>
+        <Icon
+          icon="mdi:rewind-5"
+          width="24"
+          height="24"
+          onclick={() => {
+            video.currentTime -= 5;
+          }}
+        />
+      </button>
+
+      <button>
+        <Icon icon="mdi:arrow-back" width="24" height="24" />
+      </button>
+
+      <button onclick={togglePlayback}>
+        {#if playing}
+          <Icon icon="mdi:pause" width="24" height="24" />
+        {:else}
+          <Icon icon="mdi:play" width="24" height="24" />
+        {/if}
+      </button>
+
+      <button>
+        <Icon icon="mdi:arrow-forward" width="24" height="24" />
+      </button>
+
+      <button>
+        <Icon
+          icon="mdi:fast-forward-5"
+          width="24"
+          height="24"
+          onclick={() => {
+            video.currentTime += 5;
+          }}
+        />
+      </button>
+    </div>
   </div>
-
-  <br />
-
-  <button
-    onclick={() => {
-      if (video.paused || video.ended) video.play();
-      else video.pause();
-    }}
-    >{#if video?.paused || video?.ended}Play{:else}Pause{/if}</button
-  >
 </main>
 
 <style>
@@ -37,7 +78,13 @@
   }
 
   #video-container {
+    display: flex;
+    flex-direction: column;
     width: var(--videoWidth);
+  }
+
+  #player-container {
+    width: 100%;
     height: calc(var(--videoWidth) * (9 / 16));
     anchor-name: --videoContainer;
   }
@@ -71,8 +118,20 @@
 
   .caption-line {
     text-align: center;
+    user-select: none;
     width: fit-content;
     padding: 0 0.25em;
     background: rgba(8, 8, 8, 0.75);
+  }
+
+  #button-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 8px 0;
+  }
+
+  button {
+    margin: 0 4px;
   }
 </style>
